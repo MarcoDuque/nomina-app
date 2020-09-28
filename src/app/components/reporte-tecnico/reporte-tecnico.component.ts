54953a3c115a0a301bc77771c795008feb54bf77
MarcoDuque <63019554+MarcoDuque@users.noreply.github.com>
2020-09-27 20:28:12 -0500

    interface y refac

--- src/app/components/reporte-tecnico/reporte-tecnico.component.ts
+++ src/app/components/reporte-tecnico/reporte-tecnico.component.ts
@@ -36,6 +36,7 @@ export class ReporteTecnicoComponent implements OnInit {
     (this.form.valid)
       ? this.reporteTecnico.guardarReporte(this.form.getRawValue())
         .subscribe((reporte: ReporteTecnico) => {
+          this.form.reset();
           alert('Registro guardado exitosamente');
         }, (err: HttpErrorResponse) => {
           alert('El registro no se pudo guardar');
--- src/app/models/consulta.interface.ts
+++ src/app/models/consulta.interface.ts
@@ -1,4 +1,4 @@
 export interface ConsultaReporte {
-  idTecnico: String,
-  semana: String
+  idTecnico: String;
+  semana: String;
 }
